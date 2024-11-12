8a5443c81d3384b3d76775dbae31cb3ad161bd99
minseg <artssam7602@naver.com>
2024-11-12 15:51:13 +0900

    파티클 안에 있던 숨은 중력을 빼고, applyForce 적용

--- Particle.js
+++ Particle.js
@@ -1,68 +1,29 @@
 // A simple Particle class
 
-
-    class Particle {
-        constructor(position) {
-            this.acceleration = createVector(0, 0.05);
-            this.velocity = createVector(random(-1, 1), random(-1, 0));
-            this.position = position.copy();
-            this.lifespan = 255;
-        }
-    
-    
-        run() {
-            this.update();
-            this.display();
-        }
-    
-    
-        update() {
-            this.velocity.add(this.acceleration);
-            this.position.add(this.velocity);
-            this.lifespan -= 2;
-        }
-    
-    
-        display() {
-            stroke(200, this.lifespan);
-            strokeWeight(2);
-            fill(127, this.lifespan);
-            ellipse(this.position.x, this.position.y, 12, 12);
-        }
-    
-    
-        isDead() {
-            return this.lifespan < 0;
-        }
+class Particle {
+    constructor(position) {
+        this.acceleration = createVector(0, 0);
+        this.velocity = createVector(random(-1, 1), random(-1, 0));
+        this.position = position.copy();
+        this.lifespan = 255;
     }
-    
-    
-    
-    
-    
-    
-    
-    // let Particle = function(position) {
-    
-    
-    //   };
-    
-    
-    //   Particle.prototype.run = function() {
-    //   };
-    
-    
-    //   // Method to update position
-    //   Particle.prototype.update = function(){
-    //   };
-    
-     
-      
-    //   // Method to display
-    //   Particle.prototype.display = function() {
-    //   };
-    
-    
-    //   // Is the particle still useful?
-    //   Particle.prototype.isDead = function(){
-    //   };
+
+    run() {
+        this.update();
+        this.display();
+    }
+
+    applyForce(force) {
+        this.acceleration.add(force);
+    }
+    update() {
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.lifespan -= 2;
+        this.acceleration.set(0, 0);
+    }
+
+    display() {
+@@ -31,35 +37,3 @@
+    }
+}
