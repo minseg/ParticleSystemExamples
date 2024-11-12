2c548cac90ecd5d096bb9b122a8d15f2a2628424
minseg <artssam7602@naver.com>
2024-11-12 16:01:32 +0900

    .

--- sketch.js
+++ sketch.js
@@ -5,7 +5,6 @@ let wind;
 
 function setup() {
   createCanvas(720, 400);
-  
   // system = new ParticleSystem(createVector(width / 2, 50));
   g = createVector(0, 0.05);
   wind = createVector(0.03, -0.01);
