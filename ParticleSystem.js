824189dc2a1eca6b549a9ada351696961644083a
minseg <artssam7602@naver.com>
2024-11-12 15:54:40 +0900

    wind를 적용하는 코드 추가

--- ParticleSystem.js
+++ ParticleSystem.js
@@ -13,6 +13,12 @@ class ParticleSystem {
             p.applyForce(g);
         }
     }
+
+    applyForce(force) {
+        for (let p of this.particles) {
+            p.applyForce(force);
+        }
+    }
     run() {
         for (let i = this.particles.length-1; i >= 0; i--) {
             let p = this.particles[i];
@@ -20,6 +26,8 @@ class ParticleSystem {
             if (p.isDead()) {
               this.particles.splice(i, 1);
             }
-          }
+        }
     }
 }
+
+        
